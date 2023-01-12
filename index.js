138ae929a837579b198af01b1ba5c4a4b41232c6
shabebalhosh <shabebalhosh.lb@gmail.com>
2023-01-12 21:38:27 +0200

    minor fixes

---
+++ index.js
@@ -0,0 +1,21 @@
+setTimeout(function(){
+    document.getElementById("splashP").style.display = "none";
+  }, 5300);
+  
+  document.getElementById("name").oninput = function() {
+    this.value = this.value.replace(/[^\s,أ-ي]/gi, '');
+  } 
+  
+  document.getElementById("phonenb").oninput = function() {
+    this.value = this.value.replace(/[^0-9]/gi, '');
+  }
+
+  document.getElementById("phonenb").oninput = function() {
+    if (this.value.length > 8) {
+      this.value = this.value.substring(0, 8);
+    }
+  }
+  
+  document.getElementById("name").setAttribute("autocomplete", "off");
+  
+  document.getElementById("phonenb").setAttribute("autocomplete", "off");
