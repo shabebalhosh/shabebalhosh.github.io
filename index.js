f28fe5e9d87cb65e364069b9c2e3a5c59003f881
shabebalhosh <shabebalhosh.lb@gmail.com>
2023-01-13 11:59:09 +0200

    11:59

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ setTimeout(function(){
   document.getElementById("name").setAttribute("autocomplete", "off");
   
   document.getElementById("phonenb").setAttribute("autocomplete", "off");
+  
+  
+  var md = new MobileDetect(window.navigator.userAgent);
+if (md.mobile()) {
+  // code to run if the website is accessed from a mobile device
+} else {
+  // code to run if the website is accessed from a desktop
+  window.location.href = "https://google.com";
+}
