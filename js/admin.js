de5366a874c802d684311cd36a22f6456e699ecc
shabebalhosh <shabebalhosh.lb@gmail.com>
2023-01-24 10:51:24 +0200

    huge upgrade

---
+++ js/admin.js
@@ -0,0 +1,48 @@
+import 'https://www.gstatic.com/firebasejs/7.21.1/firebase-app.js';
+import 'https://www.gstatic.com/firebasejs/7.21.1/firebase-database.js';
+import 'https://www.gstatic.com/firebasejs/7.21.1/firebase-auth.js';
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+}, false);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSOB0zMXgqb9XOzrWckFvh_OjuhkiTT5M",
+  authDomain: "contributionanddonationform.firebaseapp.com",
+  databaseURL: "https://contributionanddonationform-default-rtdb.firebaseio.com",
+  projectId: "contributionanddonationform",
+  storageBucket: "contributionanddonationform.appspot.com",
+  messagingSenderId: "965432599487",
+  appId: "1:965432599487:web:41c5c59bd1e5dc608f770a"
+};
+firebase.initializeApp(firebaseConfig);
+var loginForm = document.querySelector("#loginForm");
+loginForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  var email = document.querySelector("#email").value;
+  var password = document.querySelector("#password").value;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(function(user) {
+    document.querySelector('#loginF').style.display = "none";
+    document.querySelector("#dataTable").style.display = "";
+    var database = firebase.database();
+    var tableBody = document.querySelector("tbody");
+    database.ref('donators').on("child_added", function(snapshot) {
+      var data = snapshot.val();
+      var row = document.createElement("tr");
+      var nameCell = document.createElement("td");
+      nameCell.textContent = data.name;
+      var phoneNBCell = document.createElement("td");
+      phoneNBCell.textContent = data.phoneNB;
+      var donationTypeCell = document.createElement("td");
+      donationTypeCell.textContent = data.donationType;
+      row.appendChild(donationTypeCell);
+      row.appendChild(phoneNBCell);
+      row.appendChild(nameCell);
+      tableBody.appendChild(row);
+    }); 
+  })
+  .catch(function(error) {
+    alert(error.message);
+  });
+});
---
+++ js/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBSOB0zMXgqb9XOzrWckFvh_OjuhkiTT5M",
+  authDomain: "contributionanddonationform.firebaseapp.com",
+  databaseURL: "https://contributionanddonationform-default-rtdb.firebaseio.com",
+  projectId: "contributionanddonationform",
+  storageBucket: "contributionanddonationform.appspot.com",
+  messagingSenderId: "965432599487",
+  appId: "1:965432599487:web:41c5c59bd1e5dc608f770a"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    set(ref(db, 'donators/' + Math.random().toString(36).slice(2, 7)), {
+        name: document.getElementById('name').value,
+        phoneNB: document.getElementById('phonenb').value,
+        donationType: document.getElementById('donationType').value
+    });
+    document.getElementById('contactForm').reset();
+    alert('تم إرسال معلوماتك التي أدخلتها إلى قاعدة البيانات الخاصة بتجمّع شباب الحوش. سوف يتم التّواصل معك بأقرب وقت، شكراً لوقتك');
+});
